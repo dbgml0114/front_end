f8268cc54ff860a5648a1d694767a03d97487ccb
dbgml0114 <dbgml0114@naver.com>
2016-07-25 17:56:36 +0900

    ~7/25

--- class/webpage/js/gnb.js
+++ class/webpage/js/gnb.js
@@ -6,14 +6,18 @@
 $(function(){
 
     $('.gnb-depth1-link').on('mouseenter',function(){
-        $('.gnb-depth2').show();
-        $('.gnb-bg').show();
+        $('.gnb-depth2').stop().delay(500).fadeIn(1000);
+        $('.gnb-bg').stop().animate({
+            height:274
+        });
 
     });
 
     $('.gnb-depth1-link').on('mouseleave',function(){
-        $('.gnb-depth2').hide();
-        $('.gnb-bg').hide();
+        $('.gnb-depth2').stop().fadeOut(30);
+        $('.gnb-bg').stop().animate({
+            height:0
+        });
 
     });
 
